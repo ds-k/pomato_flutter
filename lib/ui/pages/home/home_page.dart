a7d3a16763d0b522548ac0493007e191e29885b0
Daeseong Kim <daeseongkim05@gmail.com>
2025-05-27 05:45:17 +0900

    feat: logout 및 refreshtoken 구현

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -7,10 +7,26 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    print(ref.read(authViewModelProvider).provider);
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Home Page'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Home Page'),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(authViewModelProvider.notifier).logout(context);
+              },
+              child: const Text('Logout test'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(authViewModelProvider.notifier).refreshToken();
+              },
+              child: const Text('refresh test'),
+            ),
+          ],
+        ),
       ),
     );
   }
